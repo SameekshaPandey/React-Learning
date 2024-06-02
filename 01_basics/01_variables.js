e9241cedcd56ec0f02792b565e58ac14b6348a87
SameekshaPandey <113576768+SameekshaPandey@users.noreply.github.com>
2024-06-02 14:03:11 +0000

    JS Fundamentals

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "144553";
+let accountEmail = "razee@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "raz@gmail.com"
+accountPassword = "1234"
+accountCity = "Udaipur"
+//accountId = 2
+/*Prefer not to use var bcz of block scope n functional scope*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+/*number
+bigint
+string
+boolean 
+null // let state = null;
+undefined //let state; value not assigned
+symbol //identify uniquness
+object
+typeof*/
+console.log(typeof(13)) //number
+console.log(typeof(undefined))//undefined
+console.log(typeof(null))//object
